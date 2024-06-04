1110ad2a78a844dabe31de57ca2b82546cbe6ca9
Will Plant <williamrplant@gmail.com>
2024-06-04 10:30:52 +0100

    refactor: centralised Backstage Passes logic

--- src/GildedRose/GildedRose.cs
+++ src/GildedRose/GildedRose.cs
@@ -27,54 +27,40 @@ namespace GildedRoseKata
                     continue;
                 }
 
-                if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (Items[i].Name.StartsWith("Backstage passes"))
                 {
-                    if (Items[i].Quality > 0)
+                    if (Items[i].SellIn <= 0)
                     {
-                        Items[i].Quality = Items[i].Quality - 1;
+                        Items[i].Quality = 0;
                     }
+                    else if (Items[i].SellIn <= 5)
+                    {
+                        Items[i].Quality = Math.Min(Items[i].Quality + 3, 50);
+                    }
+                    else if (Items[i].SellIn <= 10)
+                    {
+                        Items[i].Quality = Math.Min(Items[i].Quality + 2, 50);
+                    }
+                    else
+                    {
+                        Items[i].Quality = Math.Min(Items[i].Quality + 1, 50);
+                    }
+                    Items[i].SellIn -= 1;
+                    continue;
                 }
-                else
+
+                if (Items[i].Quality > 0)
                 {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
+                    Items[i].Quality = Items[i].Quality - 1;
                 }
 
                 Items[i].SellIn = Items[i].SellIn - 1;
 
                 if (Items[i].SellIn < 0)
                 {
-                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                    if (Items[i].Quality > 0)
                     {
-                        if (Items[i].Quality > 0)
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                    else
-                    {
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                        Items[i].Quality = Items[i].Quality - 1;
                     }
                 }
             }
--- src/GildedRoseTests/GildedRoseTest.cs
+++ src/GildedRoseTests/GildedRoseTest.cs
@@ -52,5 +52,36 @@ namespace GildedRoseTests
             sut.UpdateQuality();
             Assert.Equal(expectedSellIn, Items[0].SellIn);
         }
+
+        [Theory]
+        [InlineData(11, 5, 6)]
+        [InlineData(11, 50, 50)]
+        [InlineData(10, 5, 7)]
+        [InlineData(6, 5, 7)]
+        [InlineData(6, 50, 50)]
+        [InlineData(5, 5, 8)]
+        [InlineData(1, 5, 8)]
+        [InlineData(1, 50, 50)]
+        [InlineData(0, 5, 0)]
+        [InlineData(-1, 5, 0)]
+        public void BackstagePasses_Quality(int sellIn, int quality, int expectedQuality)
+        {
+            var Items = new List<Item> { new() { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellIn, Quality = quality } };
+            var sut = new GildedRose(Items);
+            sut.UpdateQuality();
+            Assert.Equal(expectedQuality, Items[0].Quality);
+        }
+
+        [Theory]
+        [InlineData(1, 5, 0)]
+        [InlineData(0, 5, -1)]
+        [InlineData(-1, 5, -2)]
+        public void BackstagePasses_SellIn(int sellIn, int quality, int expectedSellIn)
+        {
+            var Items = new List<Item> { new() { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellIn, Quality = quality } };
+            var sut = new GildedRose(Items);
+            sut.UpdateQuality();
+            Assert.Equal(expectedSellIn, Items[0].SellIn);
+        }
     }
 }
